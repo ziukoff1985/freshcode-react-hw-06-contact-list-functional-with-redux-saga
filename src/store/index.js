70b3ed8d0864d5678482b0d37fadf05c7fb48a29
Borys Ziukov <ziukoff1985@gmail.com>
2025-12-18 14:43:11 +0200

    Add Saga to store/index.js, create sagas/contactSagas.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,16 @@
 import { createStore } from 'redux';
 import { applyMiddleware } from 'redux'; // метод для додавання middleware
-
 import { composeWithDevTools } from '@redux-devtools/extension'; // для роботи Redux DevTools
 import logger from 'redux-logger'; // для middleware - logger
 
+import createSagaMiddleware from 'redux-saga'; // ! SAGA
+
 import contactsReducer from './reducers/contactsReducer';
 
-const middleware = applyMiddleware(logger); // додавання middleware logger
+const sagaMiddleware = createSagaMiddleware(); // ! SAGA
+
+const middleware = applyMiddleware(sagaMiddleware, logger); // ! додавання middleware logger i saga
 
 export default createStore(contactsReducer, composeWithDevTools(middleware));
+
+sagaMiddleware.run(); // ! SAGA
